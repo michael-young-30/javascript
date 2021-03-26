25d72d067010211919cbf91c9e39c2e7b59de34a
michael-young-30 <michaelkawikayoung@gmail.com>
2021-03-25 18:29:36 -0700

    Adding changes to js files

---
+++ static/js/app.js
@@ -0,0 +1,32 @@
+const tableData = data;
+
+const tbody=d3.select("tbody");
+
+function buildTable(data) {
+    tbody.html("");
+    data.forEach((dataRow)=>{
+        const row = tbody.append("tr");
+        Object.values(dataRow).forEach((val)=>{
+            let cell = row.append("td");
+                cell.text(val);
+        })
+    });
+}
+
+
+function handleClick() {
+    const date = d3.select("#datetime").property("value");
+    let filteredData=tableData;
+    if(date){
+
+        filteredData = filteredData.filter(row=>row.datetime===date);
+    }
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
+
+
+
